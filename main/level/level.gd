68199d2121dbbf54c57e0f5e1ff4b55383f41f0c
Arpit Srivastava <skyvastern@gmail.com>
2026-09-07 12:58:49 +0530

    Add experience stat

--- main/level/level.gd
+++ main/level/level.gd
@@ -3,6 +3,7 @@ class_name Level
 
 signal focus_changed
 signal learning_changed
+signal xp_changed
 
 var focus: int = 0:
 	set(value):
@@ -18,10 +19,18 @@ var learning: int = 1:
 		
 		_update_ui()
 
+var xp: int = 0:
+	set(value):
+		xp = value
+		xp_changed.emit()
+		
+		_update_ui()
+
 @export_group("Action")
 @export var work_btn: Button
 @export var focus_label: Label
 @export var learning_label: Label
+@export var xp_label: Label
 
 
 func _enter_tree() -> void:
@@ -38,11 +47,13 @@ func _ready() -> void:
 
 func _on_work_btn_pressed() -> void:
 	focus += learning
+	xp += learning
 
 
 func _update_ui() -> void:
 	focus_label.text = "%s F" % focus
 	learning_label.text = "Learning: %s Focus/Click" % learning
+	xp_label.text = "Experience: %s" % xp
 
 
 func increase_learning_speed(cost: int) -> void:
